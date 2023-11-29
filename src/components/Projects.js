cea2d2d249c3c96d59899f235d0d32830329b2dd
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-29 11:01:02 +0100

    control

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,9 +18,10 @@ function Projects() {
         Projects
       </p>
 
+      {/* Grid */}
       <div class='grid grid-cols-1 gap-x-0 gap-y-12 sm:gap-x-12 sm:grid-cols-3 mt-8'>
         <div
-          className='group relative h-56 w-fullsm:colspan:1'
+          className='group relative h-56 w-full sm:colspan:1'
           data-aos='fade-right'
           data-aos-duration='800'
           data-aos-delay='200'
@@ -44,70 +45,84 @@ function Projects() {
           </a>
         </div>
 
-        <a
-          href='https://jrodriguezc160.github.io/react-movie-searcher'
-          target='_blank'
-          rel='noreferrer'
-          className='group relative flex h-56 items-end overflow-hidden rounded-xl bg-white shadow-lg sm:colspan:1 transition duration-500 hover:scale-105 hover:shadow-2xl col-span-2'
+        <div
+          className='group relative h-56 w-full sm:colspan:1 col-span-2'
           data-aos='fade-left'
           data-aos-duration='1000'
           data-aos-delay='200'
         >
-          <div className='absolute inset-4 mb-8 rounded-md overflow-hidden'>
-            <img
-              src='https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              loading='lazy'
-              alt='Weather'
-              class='absolute inset-0 h-full w-full object-cover object-center transition duration-500 group-hover:scale-105'
-            />
-          </div>
+          <a
+            href='https://jrodriguezc160.github.io/react-movie-searcher'
+            target='_blank'
+            rel='noreferrer'
+            className='h-full w-full flex items-end transition duration-500 hover:scale-105 hover:shadow-2xl overflow-hidden rounded-xl bg-white shadow-lg'
+          >
+            <div className='absolute inset-4 mb-8 rounded-md overflow-hidden'>
+              <img
+                src='https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                loading='lazy'
+                alt='Movies'
+                class='absolute inset-0 h-full w-full object-cover object-center transition duration-500 group-hover:scale-105'
+              />
+            </div>
 
-          <span className='relative ml-6 mb-3 inline-block text-sm md:ml-6 md:text-lg'>
-            Movies searcher
-          </span>
-        </a>
+            <span className='relative ml-6 mb-3 inline-block text-sm md:ml-6 md:text-lg'>
+              Movies searcher
+            </span>
+          </a>
+        </div>
 
-        <a
-          href='youtube.com'
-          className='group relative flex h-56 items-end overflow-hidden rounded-xl bg-white shadow-lg sm:colspan:1 transition duration-500 hover:scale-105 hover:shadow-2xl col-span-2'
+        <div
+          className='group relative h-56 w-full sm:colspan:1 col-span-2'
           data-aos='fade-right'
-          data-aos-duration='1200'
+          data-aos-duration='1000'
           data-aos-delay='200'
         >
-          <div className='absolute inset-4 mb-8 rounded-md overflow-hidden'>
-            <img
-              src='https://emojis.wiki/thumbs/emojis/sun-behind-cloud.webp'
-              loading='lazy'
-              alt='Weather'
-              class='absolute inset-0 h-full w-full object-cover object-center transition duration-500 group-hover:scale-105'
-            />
-          </div>
+          <a
+            href='https://jrodriguezc160.github.io/react-movie-searcher'
+            target='_blank'
+            rel='noreferrer'
+            className='h-full w-full flex items-end transition duration-500 hover:scale-105 hover:shadow-2xl overflow-hidden rounded-xl bg-white shadow-lg'
+          >
+            <div className='absolute inset-4 mb-8 rounded-md overflow-hidden'>
+              <img
+                src='https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                loading='lazy'
+                alt='Portfolio'
+                class='absolute inset-0 h-full w-full object-cover object-center transition duration-500 group-hover:scale-105'
+              />
+            </div>
 
-          <span className='relative ml-6 mb-3 inline-block text-sm md:ml-6 md:text-lg'>
-            Weather app
-          </span>
-        </a>
+            <span className='relative ml-6 mb-3 inline-block text-sm md:ml-6 md:text-lg'>
+              Portfolio
+            </span>
+          </a>
+        </div>
 
-        <a
-          href='youtube.com'
-          className='group relative flex h-56 w-full items-end overflow-hidden rounded-xl bg-white shadow-lg sm:colspan:1 transition duration-500 hover:scale-105 hover:shadow-2xl'
+        <div
+          className='group relative h-56 w-full sm:colspan:1'
           data-aos='fade-left'
-          data-aos-duration='1400'
+          data-aos-duration='800'
           data-aos-delay='200'
         >
-          <div className='absolute inset-4 mb-8 rounded-md overflow-hidden'>
-            <img
-              src='https://eshop.macsales.com/blog/wp-content/uploads/2020/07/StickiesIcon-1-284x284.png'
-              loading='lazy'
-              alt='Weather'
-              class='absolute inset-0 h-full w-90% object-cover object-center mx-auto transition duration-500 group-hover:scale-105'
-            />
-          </div>
+          <a
+            href='youtube.com'
+            className='h-full w-full flex items-end transition duration-500 hover:scale-105 hover:shadow-2xl overflow-hidden rounded-xl bg-white shadow-lg'
+          >
+            <div className='absolute inset-4 mb-8 rounded-md overflow-hidden'>
+              <img
+                src='https://eshop.macsales.com/blog/wp-content/uploads/2020/07/StickiesIcon-1-284x284.png'
+                loading='lazy'
+                alt='Sticky notes'
+                class='absolute inset-0 h-full w-auto mx-auto object-cover object-center transition duration-500 group-hover:scale-105'
+              />
+            </div>
 
-          <span className='relative ml-6 mb-3 inline-block text-sm md:ml-6 md:text-lg'>
-            Notes app
-          </span>
-        </a>
+            <span className='relative ml-6 mb-3 inline-block text-sm md:ml-6 md:text-lg'>
+              Notes app
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   );
