8c64e34a1137ebbd70c3f5249f553e90ac2a3557
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-12-06 17:01:15 +0100

    dark mode solved

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -26,7 +26,7 @@ function SideBar({ mode, changeMode }) {
 
   return (
     <div className="absolute z-10 flex justify-center flex-col h-screen w-screen sm:w-auto">
-      <div className={`fixed md:left-4 left-auto right-auto mx-5 sm:mx-0 md:top-4 bottom-4 md:w-12 w-[90%] sm:w-fit-content justify-between flex md:flex-col ${mode !== "sun" ? (mode === "dawn" ? "bg-[#214d5e]" : "bg-[#061E4690] backdrop-blur-sm hover:shadow-[#125EFE30] hover:shadow-lg text-gray-100") : "bg-white"} shadow-lg transition-all duration-300 rounded-3xl hover:rounded-2xl`}
+      <div className={`fixed md:left-4 left-auto right-auto mx-5 sm:mx-0 md:top-4 bottom-4 md:w-12 w-[90%] sm:w-fit-content justify-between flex md:flex-col ${mode !== "sun" ? (mode === "dawn" ? "bg-[#214d5e]" : "bg-[#061E4690] backdrop-blur-sm hover:shadow-[#061E4640] hover:shadow-lg text-gray-100") : "bg-white"} shadow-none transition-all duration-300 rounded-3xl hover:rounded-2xl`}
       style={{justifySelf:"center", marginRight:"auto"}}>
         <SideBarIcon
           icon={<FiHome />}
@@ -57,7 +57,7 @@ function SideBar({ mode, changeMode }) {
         />
 
         <div className={`flex md:flex-col m-auto mb-1 mr-1 w-fit rounded-3xl ${
-          mode !== "sun" ? (mode === "dawn" ? "bg-[#a1e6cb90]" : "bg-[#061E46] backdrop-blur-sm") : "bg-white"
+          mode !== "sun" ? (mode === "dawn" ? "bg-[#a1e6cb90]" : "bg-[#1515eb20] backdrop-blur-sm") : "bg-white"
         } gap-1`}>
           <div
             className={`sidebar-icon mode ${mode === "sun" ? "active" : "bg-transparent"}`}
